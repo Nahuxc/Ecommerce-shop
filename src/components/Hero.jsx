f44c61eee2cec200d3c5eb2fd25f3d078a7eaa68
Damian Ponce <damianemponce@gmail.com>
2022-08-08 19:00:22 -0300

    cambios

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='hero-container'>
+        <div className='bg-container'>
+            <img src="" alt="" />
+        </div>
+        <div className='text-container'>
+            <div>
+                <h2></h2>
+                <h3></h3>
+            </div>
+            <div className='button-container'>
+                <button />
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Hero
