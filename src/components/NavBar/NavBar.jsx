239703e49ca61caafac9a984818a74a1bb802cf6
Nahuel <coronel98nahuel@gmail.com>
2022-08-08 19:32:42 -0300

    version1.0

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import "./NavBar.css"
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='Navbar'>
+      <div className='Contenido-Navbar'>
+        gaadf
+      </div>
+    </div>
   )
 }
 
